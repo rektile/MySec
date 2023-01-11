9760969e7616a751a1c53dd2b146fbb340ee77fd
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-01-11 11:51:21 +0100

    updated arguments

--- Scanners/MyNetScan/MyNetScan.py
+++ Scanners/MyNetScan/MyNetScan.py
@@ -4,13 +4,27 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Scans network for open ports')
 
-parser.add_argument("IP", help='The ip you want to scan')
-parser.add_argument("PORT", help='Port or range of ports you want to scan e.g. 0-1000')
+parser.add_argument("-i",
+                    "--ip",
+                    help="The ip you want to scan",
+                    nargs="?",
+                    required=True)
+
+parser.add_argument("-p",
+                    "--port",
+                    help="Port or range of ports you want to scan e.g. 0-1000",
+                    nargs="?",
+                    required=True)
+
+parser.add_argument("-v",
+                    "--verbose",
+                    help="Show verbose output",
+                    action='store_true')
 
 
 args = parser.parse_args()
 
-scanner = MyNetScan(args.IP, args.PORT)
+scanner = MyNetScan(args.ip, args.port, args.verbose)
 
 scanner.run()
 
