a48e3ec9b1a92d057677e8b4f5a25109ec7a3cf3
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-01-11 11:51:44 +0100

    added helper functions for argument validation

--- Scanners/MyNetScan/classes/MyNetScan.py
+++ Scanners/MyNetScan/classes/MyNetScan.py
@@ -6,41 +6,51 @@ from tabulate import tabulate
 
 class MyNetScan:
 
-    def __init__(self, _chosenIp, _chosenPort):
+    def __init__(self, _chosenIp, _chosenPort, _verbose):
 
         self.chosenIp = _chosenIp
         self.chosenPort = _chosenPort
+        self.verbose = _verbose
 
         self.ipPattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
         self.portRange = re.compile("([0-9]+)-([0-9]+)")
 
         self.addressList = []
-        self.MINPORT = 0
-        self.MAXPORT = 65535
+        self.MINPORT = None
+        self.MAXPORT = None
+        self.MINPORTALLOWED = 0
+        self.MAXPORTALLOWED = 65535
 
-
-    def checkValid(self):
-
-        # Parse port
+    def validatePort(self):
         if self.chosenPort.isdigit():
-            self.MINPORT = int(self.chosenPort)
-            self.MAXPORT = int(self.chosenPort)
+            self.MINPORT = self.MAXPORT = int(self.chosenPort)
         else:
-            # Validate the port range
-            validRange = self.portRange.search(self.chosenPort.replace("", ""))
+            validRange = self.portRange.search(self.chosenPort.replace(" ", ""))
             if validRange:
-                self.MINPORT = int(validRange.group(1))
-                self.MAXPORT = int(validRange.group(2))
+                self.MINPORT, self.MAXPORT = int(validRange.group(1)), int(validRange.group(2))
             else:
                 return False
 
-        # Parse ip
-        validIp = self.ipPattern.search(self.chosenIp.replace(" ",""))
-        if not validIp:
+        if self.MINPORT < self.MINPORTALLOWED or self.MAXPORT > self.MAXPORTALLOWED:
             return False
 
         return True
 
+    def validateIp(self):
+        validIp = self.ipPattern.search(self.chosenIp.replace(" ", ""))
+        return validIp
+
+    def checkValid(self):
+
+        # Parse port
+        if not self.validatePort():
+            return False
+
+        # Parse ip
+        if not self.validateIp():
+            return False
+
+        return True
 
 
     def run(self):
@@ -57,13 +67,16 @@ class MyNetScan:
 
     def scanIp(self):
 
-        print("Starting scan...\n")
+        print("[*] Starting scan...\n")
 
         # Init address
         address = Address(self.chosenIp)
 
         # Loop trough port range
         for port in range(self.MINPORT, self.MAXPORT + 1):
+
+            if self.verbose:
+                print(f"[*] Scanning port {port}")
 
             # Init socket we are going to use to connect to the ports
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -77,10 +90,9 @@ class MyNetScan:
 
             # Connect to port
             result = s.connect_ex((self.chosenIp, port))
-            print(port)
+
             # Check if port is open
             if result == 0:
-                print("open")
                 curPort.isOpen = True
                 try:
                     # Grab banner
